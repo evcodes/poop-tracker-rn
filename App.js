8b7c661a09568c1e9c667fae5e6b13f06a7d001d
Eddy <eddy@thinksimple.com>
2021-10-19 01:28:35 -0400

    modifying app.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello my name is Aspen and I poop a lot.</Text>
       <StatusBar style="auto" />
     </View>
   );
